b7d8c31212783e7fbf07f6a6aece686eaa2ddf82
MWANJE ISMA SSERUNKUMA <mwanjeisma5@gmail.com>
2022-06-27 21:30:22 +0300

    created admin addbook page html template for admin to create a new book

--- BOOK_MANAGEMENT/admindashboard/urls.py
+++ BOOK_MANAGEMENT/admindashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('addbook/', views.addbook, name="addbook"),
 ]
--- BOOK_MANAGEMENT/admindashboard/views.py
+++ BOOK_MANAGEMENT/admindashboard/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
     context = {1: 'hi'}
     return render(request, 'admindashboard/index.html', context)
+
+def addbook(request):
+    context = {2: 2}
+    return render(request, 'admindashboard/addbook.html', context)
