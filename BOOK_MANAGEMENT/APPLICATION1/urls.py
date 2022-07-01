402d1e29e311fd30d675d929816d53ab46c39c3e
hamzajunior <hamzasensei22@gmail.com>
2022-07-01 17:08:31 +0300

    new html files,login view, about us,new views, changes in signin and signup

--- BOOK_MANAGEMENT/APPLICATION1/urls.py
+++ BOOK_MANAGEMENT/APPLICATION1/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('signup', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
     path('signout', views.signout, name="signout"),
-
+    path('about', views.About, name="About"),
+    path('loginview', views.loginview, name="loginview"),
 ]
