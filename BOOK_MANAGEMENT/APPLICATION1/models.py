5e830f2dedb2a6158430c0763730b01a860a28b4
hamzajunior <hamzasensei22@gmail.com>
2022-07-07 10:37:07 +0300

    work on signin and sign up

--- BOOK_MANAGEMENT/APPLICATION1/models.py
+++ BOOK_MANAGEMENT/APPLICATION1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.urls import get_user_mode
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    borrower_id = models.CharField(max_length=100,null=True,blank=True)
+    Uploaded = models.DateTimeField(auto_now=True)
+    Due_date = models.DateTimeField(auto_now=True)
+    books= models.FileField(upload_to='BOOK_MANAGEMENT/books/')
+    bookcover=models.ImageField(upload_to='BOOK_MANAGEMENT/books/covers')
+    def __str__(self):
+        return self.title 
+     
