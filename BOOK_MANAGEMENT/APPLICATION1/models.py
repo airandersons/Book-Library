c6d0b159a615d787c71e4f396efd2cf9e3cd932b
hamzajunior <hamzasensei22@gmail.com>
2022-07-07 14:08:04 +0300

    models and database

--- BOOK_MANAGEMENT/APPLICATION1/models.py
+++ BOOK_MANAGEMENT/APPLICATION1/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-from django.urls import get_user_mode
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
