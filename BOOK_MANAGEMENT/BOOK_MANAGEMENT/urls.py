45e0636e846fc41dfd42ad8f12179bb1ee700c97
MWANJE ISMA SSERUNKUMA <mwanjeisma5@gmail.com>
2022-07-18 09:48:03 +0300

    added static_root in settings.py

--- BOOK_MANAGEMENT/BOOK_MANAGEMENT/urls.py
+++ BOOK_MANAGEMENT/BOOK_MANAGEMENT/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('APPLICATION1.urls')),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
