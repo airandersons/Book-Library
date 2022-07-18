7f33011f056cad9326e310e733e3b5fe4c169a3a
MWANJE ISMA SSERUNKUMA <mwanjeisma5@gmail.com>
2022-07-18 10:01:36 +0300

    added host domain name in allowed_hosts

--- BOOK_MANAGEMENT/BOOK_MANAGEMENT/settings.py
+++ BOOK_MANAGEMENT/BOOK_MANAGEMENT/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#d6mwowtb6bcy^$ayhc^+d(nx+^@nc59vg$j&6i#(9gjqlc4&7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://muk-library.herokuapp.com']
 
 
 # Application definition
